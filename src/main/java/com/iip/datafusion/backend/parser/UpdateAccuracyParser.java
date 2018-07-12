ae6aec1c8d70322a437ada39e676aea469288fb7
cq <736503360@qq.com>
2018-07-12 13:08:05 +0800

    准确性更新

--- src/main/java/com/iip/datafusion/backend/parser/UpdateAccuracyParser.java
+++ src/main/java/com/iip/datafusion/backend/parser/UpdateAccuracyParser.java
@@ -1,15 +1,23 @@
 package com.iip.datafusion.backend.parser;
 
+import com.iip.datafusion.backend.jdbchelper.JDBCHelper;
 import com.iip.datafusion.backend.job.accuracy.UpdateAccuracyJob;
 import com.iip.datafusion.dgs.model.accuracy.KeyValue;
 import com.iip.datafusion.dgs.model.accuracy.UpdateAccuracyConfiguration;
+import com.iip.datafusion.util.dbutil.DataSourceRouterManager;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.jdbc.support.rowset.SqlRowSetMetaData;
 
 import java.security.Key;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class UpdateAccuracyParser implements Parser {
+
+    private final static JdbcTemplate jdbcTemplate = JDBCHelper.getJdbcTemplate();
 
     public static UpdateAccuracyJob parser(UpdateAccuracyConfiguration updateAccuracyConfiguration) throws Exception{
 
@@ -17,6 +25,19 @@ public class UpdateAccuracyParser implements Parser {
         List<KeyValue> newValues = updateAccuracyConfiguration.getNewValues();
 
         List<String> sqlList = new ArrayList<>();
+
+        DataSourceRouterManager.setCurrentDataSourceKey(updateAccuracyConfiguration.getDataSourceId());
+
+        Map<String,String> columnType = new HashMap<>();
+        String sqltype = String.format("SELECT * FROM %s LIMIT 0",updateAccuracyConfiguration.getTableName());
+        SqlRowSet sqlRowSet = jdbcTemplate.queryForRowSet(sqltype);
+        SqlRowSetMetaData sqlRsmd = sqlRowSet.getMetaData();
+        for(int i=1;i<=sqlRsmd.getColumnCount();i++){
+            columnType.put(sqlRsmd.getColumnName(i),sqlRsmd.getColumnTypeName(i));
+        }
+
+        System.out.println(columnType);
+
 
         if (attributeValues == null || attributeValues.size() <= 0) {
             throw new Exception("传入记录不能为空");
@@ -48,7 +69,13 @@ public class UpdateAccuracyParser implements Parser {
                 String value = attributeValue.get(j).getValue();
                 if (key != null && key.trim() != "") {
                     columnClause += key + ",";
-                    valueClause += value + ",";
+                    //if(columnType.get(key).equals("CHAR")||columnType.get(key).equals("VARCHAR")||columnType.get(key).equals("TEXT")||columnType.get(key).equals("LONGTEXT")){
+                    if(columnType.get(key).contains("CHAR")||columnType.get(key).contains("TEXT")){
+                        valueClause += "'" + value + "'" + ",";
+                    }
+                    else{
+                        valueClause += value + ",";
+                    }
                 }
                 else{
                     throw new Exception("属性不能为空");
@@ -61,7 +88,7 @@ public class UpdateAccuracyParser implements Parser {
             valueClause += ");";
 
             String sql = String.format("REPLACE INTO %s %s VALUES %s",updateAccuracyConfiguration.getTableName(),columnClause,valueClause);
-            System.out.println(sql);
+            //System.out.println(sql);
             sqlList.add(sql);
         }
 
